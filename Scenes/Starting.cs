c22ccc4f610719440852d7ff51a27845a2bf35a5
Senhung Wong <alexwongsenhung@gmail.com>
2021-01-03 21:24:49 +0800

    Add build configs + fix bgm + convert wav to ogg

--- Scenes/Starting.cs
+++ Scenes/Starting.cs
@@ -2,12 +2,12 @@ using Godot;
 
 namespace DungeonGuidance.Scenes
 {
-    public class Starting : Node2D
-    {
-        public override void _Input(InputEvent @event)
-        {
-            if (!(@event is InputEventKey key) || key.Pressed) return;
-            GetTree().ChangeScene("res://Scenes/Lv1MovementTutorial.tscn");
-        }
-    }
+	public class Starting : Node2D
+	{
+		public override void _Input(InputEvent @event)
+		{
+			if (!(@event is InputEventKey key) || key.Pressed) return;
+			GetTree().ChangeScene("res://Scenes/Lv1MovementTutorial.tscn");
+		}
+	}
 }
